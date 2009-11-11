6633c76a857357cc309daccead5bffa63c1faedb
Pete Hodgson <phodgson@lyris.com>
2009-11-11 11:26:41 -0800

    More tweaks to logger_example

--- examples/logger_example.rb
+++ examples/logger_example.rb
@@ -8,12 +8,14 @@ end
 
 
 Gimme.configure do |g|
-  g.for_a( :logger ) do |env,*args|
-    Logger.new( env[:logging_threshold], *args )
+  g.for_a( Logger ) do |env, name|
+    name ||= 'default'
+    Logger.new( env[:logging_threshold], name )
   end
 end
 
 Gimme.environment = {:logging_threshold => :INFO}
 
 
-logger = Gimme.a(:logger,'Dave')
+default_logger = Gimme.a(Logger)
+dave_logger = Gimme.a(Logger,'Dave')
