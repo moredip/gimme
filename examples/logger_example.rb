07712875df730f09568e014e69a43936f9e46f04
Pete Hodgson <phodgson@lyris.com>
2009-11-11 10:26:42 -0800

    Tweaked logger example

--- examples/logger_example.rb
+++ examples/logger_example.rb
@@ -1,19 +1,19 @@
 require 'gimme'
 
 class Logger
-  def initialize( threshold )
-    puts "creating a logger with a threshold of #{threshold}"
+  def initialize( threshold, name )
+    puts "creating a logger called #{name} with a threshold of #{threshold}"
   end
 end
 
 
 Gimme.configure do |g|
-  g.for_a( :logger ) do |env|
-    Logger.new( env[:logging_threshold] )
+  g.for_a( :logger ) do |env,*args|
+    Logger.new( env[:logging_threshold], *args )
   end
 end
 
 Gimme.environment = {:logging_threshold => :INFO}
 
 
-logger = Gimme.a(:logger)
+logger = Gimme.a(:logger,'Dave')
